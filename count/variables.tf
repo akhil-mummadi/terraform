a7c8457f76c1f1c4db78d90f0ff6e0453290762b
Akhil Mummadi <akhilsp234@gmail.com>
2026-03-04 14:50:22 +0530

    count loop

---
+++ count/variables.tf
@@ -0,0 +1,23 @@
+variable "instances" {
+    type = list
+    default = ["mongodb", "redis", "mysql", "rabbitmq", "catalogue", "user", "cart", "shipping", "payment", "frontend"]
+}
+
+# replace with your zone_id and domain_name
+variable "zone_id" {
+    default = "Z00954021W0BUF6UKXY29"
+}
+
+variable "domain_name" {
+    default = "dawsakhilmummadi.online"
+}
+
+variable "fruits" {
+    type = list(string)
+    default = ["apple", "banana", "apple", "orange"]
+}
+
+variable "fruits_set" {
+    type = set(string)
+    default = ["apple", "banana", "apple", "orange"]
+}
